ca08b4dda4c845ff9cf4e8a0e024d49d19823af3
Monali9976 <114649777+Monali9976@users.noreply.github.com>
2025-08-10 12:11:20 +0530

    Update materialController.js

--- src/controllers/materialController.js
+++ src/controllers/materialController.js
@@ -1,35 +1,3 @@
-// const fs = require('fs');
-// const path = require('path');
-// const Material = require('../models/Material');
-// const { parsePDF } = require('../services/fileParser');
-// const { chunkText } = require('../utils/chunking');
-
-// async function uploadMaterial(req, res) {
-//   try {
-//     if (!req.file) return res.status(400).json({ ok: false, error: 'no file' });
-//     const filePath = req.file.path;
-//     const pages = await parsePDF(filePath);
-//     const docs = [];
-//     for (let i = 0; i < pages.length; i++) {
-//       const pageText = pages[i];
-//       const chunks = chunkText(pageText, 1500);
-//       for (const ch of chunks) {
-//         docs.push({ materialName: req.file.originalname, pageNumber: i + 1, text: ch });
-//       }
-//     }
-//     await Material.insertMany(docs);
-//     fs.unlinkSync(filePath);
-//     res.json({ ok: true, indexed: docs.length });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ ok: false, error: err.message });
-//   }
-// }
-
-// module.exports = { uploadMaterial };
-
-
-
 const pdfParse = require("pdf-parse");
 const Material = require("../models/Material");
 
